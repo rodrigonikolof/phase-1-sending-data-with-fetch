ad819a43b22e817497387cc3090c9f810493f670
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-07 10:51:24 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+    "Content-Type": 'application/json',
+    "Accept" : 'application/json',
+    },
+    body: JSON.stringify({
+        'name' : name, 
+        'email' : email,
+    }),   
+})
+    .then(response => response.json())
+    .then(json => {
+       let p = document.createElement('p');
+       p.textContent += `The logged ID is ${json.id}.`
+       document.body.appendChild(p);
+    })
+    .catch(message => {
+        let errorP = document.createElement('p');
+        errorP.textContent = `Sorry bro here is an error: ${message}`;
+        document.body.appendChild(errorP)
+    })
+
+}
+
+// console.log(submitData('Rodrigo', '456'));
